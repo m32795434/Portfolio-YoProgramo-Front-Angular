171b66b8fcb87413f56aa254a82a69661f6c3e32
Manuel Augusto Bravard <manubravard@gmail.com>
2023-03-07 12:51:03 -0300

    style scss location fixed + router working cool

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { QPDComponent } from './components/qpd/qpd.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'qPD', component: QPDComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
