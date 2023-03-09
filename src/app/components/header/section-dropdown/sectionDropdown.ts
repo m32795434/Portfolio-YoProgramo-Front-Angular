7f53b7f69dfb2417f3869582e1bf7a6aa71d5991
Manuel Augusto Bravard <manubravard@gmail.com>
2023-03-09 01:57:19 -0300

    cleaning!

--- src/app/components/header/section-dropdown/sectionDropdown.ts
+++ src/app/components/header/section-dropdown/sectionDropdown.ts
@@ -9,13 +9,17 @@ import { RouterModule } from '@angular/router';
     imports: [NgbDropdownModule, RouterModule],
     templateUrl: './sectionDropdown.html',
     styles: [`
+    
     #menu-Hamb {
-    // font-family: 'Material Icons';
     color: #b3b3b3;
     font-size: 20px;
     background-color: transparent;
     outline: none;
     border: transparent;
+    &:hover {
+            border-radius: 7px;
+            box-shadow: 0 0 7px 4px #8a8989;
+        }
 }
     #sectionBtn {
         font-family: Montserrat, 'Open Sans', Lato, 'sans-serif';
@@ -30,10 +34,12 @@ import { RouterModule } from '@angular/router';
 })
 export class NgbdDropdownBasic implements OnInit {
     constructor() { }
+
     ngOnInit(): void {
         window.onresize = this.checkForResize;
         this.checkForResize();
     }
+
     text = 'Section';
     greaterThan975 = false;
 
@@ -41,30 +47,13 @@ export class NgbdDropdownBasic implements OnInit {
         const widthViewPort: any = window?.visualViewport?.width;
         if (widthViewPort != null) {
             if (widthViewPort > 975.2) {
-                // window.location.reload();
-                console.log("widthViewPort > 975.2")
                 this.text = 'Section';
                 this.greaterThan975 = true;
-                console.log(this.text)
             } else if (widthViewPort < 975.2) {
-                // window.location.reload();
-                console.log("widthViewPort < 975.2")
                 this.text = 'menu';
                 this.greaterThan975 = false;
-                console.log(this.text)
             }
         }
 
     }
 }
-/*CHECK FOR RESIZE!
-widthViewPort = window.visualViewport.width;
-
-if (widthViewPort < 975.2) {
-myExperienceSlider = new Slider1(
-document.querySelector('.slider-experience')
-);
-}
-
-
-*/
