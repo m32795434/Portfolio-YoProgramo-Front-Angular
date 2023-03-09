fc8972b7bfa7ad4cc7a3819092ea8dd074d4ebc9
Manuel Augusto Bravard <manubravard@gmail.com>
2023-03-09 01:50:13 -0300

    toggle Section and Menu hamb

--- src/app/components/header/section-dropdown/sectionDropdown.ts
+++ src/app/components/header/section-dropdown/sectionDropdown.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 
@@ -9,6 +9,14 @@ import { RouterModule } from '@angular/router';
     imports: [NgbDropdownModule, RouterModule],
     templateUrl: './sectionDropdown.html',
     styles: [`
+    #menu-Hamb {
+    // font-family: 'Material Icons';
+    color: #b3b3b3;
+    font-size: 20px;
+    background-color: transparent;
+    outline: none;
+    border: transparent;
+}
     #sectionBtn {
         font-family: Montserrat, 'Open Sans', Lato, 'sans-serif';
         font-size: 20px;
@@ -20,5 +28,43 @@ import { RouterModule } from '@angular/router';
         }`]
 
 })
-export class NgbdDropdownBasic {
+export class NgbdDropdownBasic implements OnInit {
+    constructor() { }
+    ngOnInit(): void {
+        window.onresize = this.checkForResize;
+        this.checkForResize();
+    }
+    text = 'Section';
+    greaterThan975 = false;
+
+    checkForResize = (): void => {
+        const widthViewPort: any = window?.visualViewport?.width;
+        if (widthViewPort != null) {
+            if (widthViewPort > 975.2) {
+                // window.location.reload();
+                console.log("widthViewPort > 975.2")
+                this.text = 'Section';
+                this.greaterThan975 = true;
+                console.log(this.text)
+            } else if (widthViewPort < 975.2) {
+                // window.location.reload();
+                console.log("widthViewPort < 975.2")
+                this.text = 'menu';
+                this.greaterThan975 = false;
+                console.log(this.text)
+            }
+        }
+
+    }
 }
+/*CHECK FOR RESIZE!
+widthViewPort = window.visualViewport.width;
+
+if (widthViewPort < 975.2) {
+myExperienceSlider = new Slider1(
+document.querySelector('.slider-experience')
+);
+}
+
+
+*/
