99b2e31dec2c201873c5db5660f460eab895dd2c
Manuel Augusto Bravard <manubravard@gmail.com>
2023-03-11 10:37:03 -0300

    Link to the socials!

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  imgEventHandler(e: any) {
+    console.log(e);
+    if (e.key === 'Enter' || e.type === 'click') {
+      console.log(e.target.alt);
+      switch (e.target.alt) {
+        case 'Instagram':
+          window.location.assign('https://www.instagram.com/augustox86/');
+          break;
+        case 'Linkedin':
+          window.location.assign('https://www.linkedin.com/in/manuel-augusto-bravard-642034204/');
+          break;
+        case 'Whatsapp':
+          window.location.assign('https://api.whatsapp.com/send?phone=5493454093473')
+          break;
+        case 'Argentina Programa':
+          window.location.assign('https://www.argentina.gob.ar/economia/conocimiento/argentina-programa');
+          break;
+        default:
+          window.location.assign('https://api.whatsapp.com/send?phone=5493454093473');
+          break;
+      }
+    }
+  }
+
 }
