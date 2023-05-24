d630aee2c5d547b490d048e06a9304448bfac093
Manuel Augusto Bravard <manubravard@gmail.com>
2023-05-23 23:33:45 -0300

    good transitions!

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -42,29 +42,37 @@ export class FooterComponent implements OnInit {
 }
 function initObserver() {
   const typerHeader: any = document.querySelector('.typer-header');
-  // const body: any = document.querySelector('body');
-  const links = Array.from(document.querySelectorAll('.footer-a'))
-  function obCallback(entries: any, ob: any) {
+  const links = Array.from(document.querySelectorAll('.footer-a'));
+  let halfBlack: any;
+  let toFadeRightEls: any;
+  let length: any;
+  async function obCallback(entries: any, ob: any) {
     entries.forEach(async (entry: any) => {
       if (entry.isIntersecting) {
-        console.log('applying classes!');
-        // ob.unobserve(terms.lastElementChild); // scape
         links.forEach((link) => { link.classList.add('bigger') })
+        halfBlack = document.getElementById("main-Half-BackG");
+        toFadeRightEls = Array.from(document.querySelectorAll('.toFadeRight'));
+        length = toFadeRightEls.length;
+        for (let i = 0; i < length; i++) {
+          toFadeRightEls[i].classList.add('fadeRight')
+          await wait(100);
+        }
+        halfBlack?.classList.add('fadeRight');
         await wait(2000);
         links.forEach((link) => { link.classList.remove('bigger') })
-
       }
-      // else {
-      //   console.log('removing classes');
-      //   links.forEach((link) => { link.classList.remove('bigger') })
-      // }
+      else {
+        console.log('removing class')
+        halfBlack?.classList.remove('fadeRight');
+        for (let i = 0; i < length; i++) {
+          await wait(200);
+          toFadeRightEls[i].classList.remove('fadeRight')
+        }
+      }
     });
   }
   const ob = new IntersectionObserver(obCallback);
-  console.log('ob', ob);
-  console.log('typerHeader', typerHeader);
   if (typerHeader) {
-    console.log('typerHeader.lastElementChild', typerHeader.lastElementChild)
     ob.observe(typerHeader.lastElementChild);
   }
 }
